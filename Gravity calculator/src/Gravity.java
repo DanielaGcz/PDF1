918112a6067d8bb615c7970d0433452f1cd124b0
Daniela_2 <Daniela_2@10.33.193.152>
2015-10-10 13:01:42 -0500

    subir

---
+++ Gravity calculator/src/Gravity.java
@@ -0,0 +1,19 @@
+
+public class Gravity {
+
+	public static void main(String[] args) {
+		double gravity = -9.81;
+		double initialVelocity = 0.0;
+		double fallingTime = 10.0;
+		double initialPosition = 0.0;
+		double finalPosition = 0.5*(gravity*fallingTime*fallingTime)+initialVelocity*fallingTime+initialPosition;
+		//x(t) = 0.5 × at2 + vit + xi
+		
+		System.out.println("The object's position after" + fallingTime + 
+				"seconds is" + finalPosition+ "m");
+
+	
+			
+			}
+		
+	}
